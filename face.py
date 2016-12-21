dfeb265de0b545b25ac6df5363fa9f777ae7e3e8
chew-yean yam <ryubidragonfire@users.noreply.github.com>
2016-12-21 15:48:31 +0000

    capture from camera

--- face.py
+++ face.py
@@ -7,7 +7,7 @@ Created on Tue Dec 20 17:44:41 2016
 
 import cv2
 
-cv2.namedWindow("preview")
+cv2.namedWindow("camera")
 vc = cv2.VideoCapture(0)
 
 if vc.isOpened(): # try to get the first frame
@@ -15,9 +15,21 @@ if vc.isOpened(): # try to get the first frame
 else:
     rval = False
 
+counter = 0
+
 while rval:
-    cv2.imshow("preview", frame)
+    cv2.imshow("camera", frame)
     rval, frame = vc.read()
-    if cv2.waitKey(20) == 27: # exit on ESC
+
+    if cv2.waitKey(1) == 27: # exit on ESC
         break
-cv2.destroyWindow("preview")
+    
+    elif cv2.waitKey(1) == 32: # save frame on Spacebar
+        # save the frame to an image file
+        filename = "face"+str(counter)+".jpg"
+        print(filename)
+        cv2.imwrite(filename, frame)
+        counter = counter + 1
+    
+vc.release()
+cv2.destroyWindow("camera")
